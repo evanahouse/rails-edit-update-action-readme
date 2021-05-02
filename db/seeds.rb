4f39636973619f06ca3a85197d923cd31c3b906a
Evan <evanahouse@gmail.com>
2021-05-01 23:13:59 -0700

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ Article.destroy_all
+
+ Article.create(title: 'Harry Potter', description:"very nice")
+ Article.create(title: 'Holy Moly', description:"Magic Cow")
+ Article.create(title: 'Surfing Bird', description:"birds the word")
+ Article.create(title: 'Water Drinking', description:"Very good for the soul")
